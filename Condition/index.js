9bbb040a369645db9faba94f9c37779fdbdd5c82
Imran-nazeer1 <imranofficial1106@gmail.com>
2026-05-14 17:43:23 +0530

    Add loop

---
+++ Condition/index.js
@@ -0,0 +1,8 @@
+console.log("Get ready to do conditional in js")
+let age = 24;
+if (age>30){
+    console.log("you can drive");
+}
+else{
+    console.log("you cannot drive");
+}
---
+++ Loops/script.js
@@ -0,0 +1,17 @@
+// for loops 
+
+console.log("hi");
+
+// for(let a=1;a<=100;a++){
+//     console.log(a);
+// }
+
+// for in loop 
+let obj ={
+    name:"Raj",
+    age:23
+}
+
+for (let key in obj) { 
+    console.log(key)
+}
