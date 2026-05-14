fb093709b88532167a6c359c055750c5baeec427
Imran-nazeer1 <imranofficial1106@gmail.com>
2026-05-14 12:25:08 +0530

    Add some more code

---
+++ Variable/style.js
@@ -0,0 +1,17 @@
+// alert("HI work now");
+
+let a =23;
+console.log(a);
+
+let obj ={
+    name:"Raj",
+    age:23
+}
+console.log(obj);
+
+obj.salary="500 crore";
+console.log(obj);
+
+
+obj.name="Imran";
+console.log(obj);
